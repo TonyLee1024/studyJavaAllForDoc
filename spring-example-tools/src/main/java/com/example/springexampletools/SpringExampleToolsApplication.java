343b5ff86091c97b206e59060fd11b7cc6c54ed7
Tony <1427406723@qq.com>
2020-07-23 10:51:04 +0800

    1-StringUtilsTool isBlank

---
+++ spring-example-tools/src/main/java/com/example/springexampletools/SpringExampleToolsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springexampletools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExampleToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringExampleToolsApplication.class, args);
+    }
+
+}
---
+++ spring-example-tools/src/main/java/com/example/springexampletools/StringTool/StringUtilsTool.java
@@ -0,0 +1,25 @@
+package com.example.springexampletools.StringTool;
+import org.apache.commons.lang3.StringUtils;
+/**
+ * @version 1.0
+ * @ClassName: String
+ * @Author lee
+ * @date 2020/7/23 10:37
+ */
+public class StringUtilsTool {
+    /**
+     * 一方面判断了字符串“null”，另一方面对参数个数无限制，只要有一个参数是空则返回true。
+     * @param args
+     * @return
+     */
+    public static boolean isNullStr(String... args){
+        boolean flag = false;
+        for (String arg : args){
+            if(StringUtils.isBlank(arg)||arg.equals("null")){
+                flag = true;
+                return flag;
+            }
+        }
+        return flag;
+    }
+}
